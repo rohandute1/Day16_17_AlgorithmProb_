9e0e08c5a95f1affed25816e5f2e0655571e6ca8
Rohan <rohandute1@gmail.com>
2023-06-21 23:11:30 +0530

    [Rohan] Successfully doen permutations of string using iterative,recursion method.

---
+++ Day16_17_AlgorithmProb/PermutationGenerator.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16_17_AlgorithmProb
+{
+    internal class PermutationGenerator
+    {
+        // Iterative method to generate permutations of a string
+        public static string[] GeneratePermutationsIterative(string str)
+        {
+            // Convert the string into a character array
+            char[] characters = str.ToCharArray();
+
+            // Initialize a list to store the permutations
+            List<string> permutations = new List<string>();
+
+            // Add the initial string to the list
+            permutations.Add(new string(characters));
+
+            // Find all permutations using Heap's algorithm
+            int[] indices = new int[str.Length];
+            int i = 0;
+
+            while (i < str.Length)
+            {
+                if (indices[i] < i)
+                {
+                    // Swap characters based on the parity of i
+                    if (i % 2 == 0)
+                        Swap(ref characters[0], ref characters[i]);
+                    else
+                        Swap(ref characters[indices[i]], ref characters[i]);
+
+                    permutations.Add(new string(characters));
+
+                    indices[i]++;
+                    i = 0;
+                }
+                else
+                {
+                    indices[i] = 0;
+                    i++;
+                }
+            }
+
+            return permutations.ToArray();
+        }
+
+        // Recursive method to generate permutations of a string
+        public static string[] GeneratePermutationsRecursive(string str)
+        {
+            List<string> permutations = new List<string>();
+
+            GeneratePermutationsRecursiveHelper(str.ToCharArray(), str.Length, permutations);
+
+            return permutations.ToArray();
+        }
+
+        // Recursive helper function
+        private static void GeneratePermutationsRecursiveHelper(char[] characters, int length, List<string> permutations)
+        {
+            if (length == 1)
+            {
+                permutations.Add(new string(characters));
+            }
+            else
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    GeneratePermutationsRecursiveHelper(characters, length - 1, permutations);
+
+                    if (length % 2 == 1)
+                    {
+                        Swap(ref characters[0], ref characters[length - 1]);
+                    }
+                    else
+                    {
+                        Swap(ref characters[i], ref characters[length - 1]);
+                    }
+                }
+            }
+        }
+
+        // Helper function to swap two characters
+        private static void Swap(ref char a, ref char b)
+        {
+            char temp = a;
+            a = b;
+            b = temp;
+        }
+
+        // Helper method to check if two arrays are equal
+        public static bool ArraysAreEqual(string[] array1, string[] array2) => array1.SequenceEqual(array2);
+
+        // Helper method to print the permutations
+        public static void PrintPermutations(string message, string[] permutations)
+        {
+            Console.WriteLine(message);
+            foreach (string permutation in permutations)
+            {
+                Console.WriteLine(permutation);
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Day16_17_AlgorithmProb/Program.cs
+++ Day16_17_AlgorithmProb/Program.cs
@@ -11,6 +11,32 @@ namespace Day16_17_AlgorithmProb
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Algorithm Programs");
+            Console.WriteLine("Please choose any one option from following");
+            Console.WriteLine(" 1.Permutation generator\n ");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Please enter any string to do permutation");
+                    string input = Console.ReadLine();
+
+                    // Generate permutations using iterative method
+                    string[] permutationsIterative = PermutationGenerator.GeneratePermutationsIterative(input);
+
+                    // Generate permutations using recursive method
+                    string[] permutationsRecursive = PermutationGenerator.GeneratePermutationsRecursive(input);
+
+                    // Check if the arrays are equal
+                    bool arraysAreEqual = PermutationGenerator.ArraysAreEqual(permutationsIterative, permutationsRecursive);
+
+                    // Print permutations and array equality
+                    PermutationGenerator.PrintPermutations("Permutations generated using the iterative method:", permutationsIterative);
+                    PermutationGenerator.PrintPermutations("Permutations generated using the recursive method:", permutationsRecursive);
+                    Console.WriteLine("Are the arrays equal? " + arraysAreEqual);
+                    break;
+
+            }
             Console.ReadLine();
         }
     }
