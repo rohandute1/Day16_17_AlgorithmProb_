a9b5069aacfa49ca81a9e800ac3e276fc1828667
Rohan <rohandute1@gmail.com>
2023-06-21 23:06:02 +0530

    [Rohan] Successfully added Welcome message

---
+++ Day16_17_AlgorithmProb/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16_17_AlgorithmProb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Programs");
+            Console.ReadLine();
+        }
+    }
+}
